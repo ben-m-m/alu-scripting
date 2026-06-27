4de263a6a88a80bf758fc627a2c62fabbd982e29
ben-m-m <mainaben23@gmail.com>
2026-06-27 15:30:07 +0200

    Refactor docstrings and improve error handling in number_of_subscribers function

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-""" Reddit API
-Queries the Reddit API.
-returns the number of subscribers
-for a given subreddit.
-"""
+"""Queries Reddit API and returns subscriber count of a subreddit."""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a subreddit."""
+    """Return number of subscribers for a subreddit or 0 if invalid."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        "User-Agent": "python:alu.reddit.api:v1.0 (by /u/anonymous)"
+        "User-Agent": "python:api_advanced:v1.0 (by /u/anonymous)"
     }
 
     try:
@@ -25,7 +22,7 @@ def number_of_subscribers(subreddit):
         if response.status_code != 200:
             return 0
 
-        return response.json()["data"]["subscribers"]
+        return response.json().get("data", {}).get("subscribers", 0)
 
     except Exception:
         return 0
