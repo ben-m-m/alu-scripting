93f7ffc1cb25c46445df0adf19520305dd332df9
ben-m-m <mainaben23@gmail.com>
2026-06-27 19:05:33 +0200

    Update User-Agent header in number_of_subscribers function

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Return total subscribers for subreddit, or 0 if invalid."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "linux:alu.api.advanced:v1.0 (by /u/oteniyatobi)"}
+    headers = {"User-Agent": "linux:alu.api.advanced:v1.0 (by /u/Web_Dev_24)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         return response.json().get("data", {}).get("subscribers", 0)
